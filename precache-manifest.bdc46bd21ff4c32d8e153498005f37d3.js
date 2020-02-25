a7700d14f4a1feeae8b91398e8befb56a79588ab
Karen Huynh <karenhuynh@karenhuynh-macbookpro.roam.corp.google.com>
2020-02-25 03:45:42 -0800

    Updates

---
+++ precache-manifest.bdc46bd21ff4c32d8e153498005f37d3.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b556d6964b501f6f035203ea9519c09",
+    "url": "/migreat/index.html"
+  },
+  {
+    "revision": "ee59e044c8801fe1bbcf",
+    "url": "/migreat/static/css/main.8c1ab618.chunk.css"
+  },
+  {
+    "revision": "a238923dbdd30bb76724",
+    "url": "/migreat/static/js/2.43e603c6.chunk.js"
+  },
+  {
+    "revision": "2f0ee2faf55c5eb9ea9267046a5519af",
+    "url": "/migreat/static/js/2.43e603c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee59e044c8801fe1bbcf",
+    "url": "/migreat/static/js/main.130c6ca2.chunk.js"
+  },
+  {
+    "revision": "3e27feeffdeb649117a5",
+    "url": "/migreat/static/js/runtime-main.d4362a75.js"
+  },
+  {
+    "revision": "d72b2e5b3324ad1e16c5400bde37d613",
+    "url": "/migreat/static/media/anon.d72b2e5b.png"
+  },
+  {
+    "revision": "362233f150216e5d15188214803dfb67",
+    "url": "/migreat/static/media/birds.362233f1.png"
+  },
+  {
+    "revision": "183addf0f03adf18c8953e102a3312c2",
+    "url": "/migreat/static/media/james1.183addf0.jpg"
+  },
+  {
+    "revision": "50504f5b55891e6b35a1f4d83930e346",
+    "url": "/migreat/static/media/james2.50504f5b.jpg"
+  },
+  {
+    "revision": "2208d0d1c06db7b395ca46e657f832be",
+    "url": "/migreat/static/media/james3.2208d0d1.jpg"
+  },
+  {
+    "revision": "060c5a8f59e0ceba625bfec36192c7f6",
+    "url": "/migreat/static/media/karen1.060c5a8f.jpg"
+  },
+  {
+    "revision": "b8e86ffa4221401697921fb6941f8b75",
+    "url": "/migreat/static/media/karen2.b8e86ffa.jpg"
+  },
+  {
+    "revision": "0d0ce99b58deae009ef3a3b3b5de4775",
+    "url": "/migreat/static/media/nidhi1.0d0ce99b.jpg"
+  },
+  {
+    "revision": "f4ff9f213e0408b6c8c9a20684ccf829",
+    "url": "/migreat/static/media/nidhi2.f4ff9f21.jpg"
+  },
+  {
+    "revision": "883c92c55495d54ded4f0a03f1588bfc",
+    "url": "/migreat/static/media/nidhi3.883c92c5.jpg"
+  },
+  {
+    "revision": "5b947656e2f46c473a28f69d008f6aa7",
+    "url": "/migreat/static/media/place1.5b947656.jpg"
+  },
+  {
+    "revision": "bba5843443f14c6e17c1dbaaf16077a4",
+    "url": "/migreat/static/media/place2.bba58434.jpg"
+  },
+  {
+    "revision": "aefb91beaf99c07b4e2f52d99f63face",
+    "url": "/migreat/static/media/place3.aefb91be.jpg"
+  },
+  {
+    "revision": "57b430419cc05db7f468306f451705a8",
+    "url": "/migreat/static/media/place4.57b43041.jpg"
+  },
+  {
+    "revision": "9fc93202af8802b06be45ebf6b55e04e",
+    "url": "/migreat/static/media/place5.9fc93202.jpg"
+  },
+  {
+    "revision": "51c1f5af02b7ef49311ec91f4a7f7ca9",
+    "url": "/migreat/static/media/place6.51c1f5af.jpg"
+  },
+  {
+    "revision": "ff12dfc2ec858de9be6906ccc534c6f2",
+    "url": "/migreat/static/media/place7.ff12dfc2.jpg"
+  },
+  {
+    "revision": "0f91ff5eefb3962d00a825933c34874b",
+    "url": "/migreat/static/media/winston1.0f91ff5e.jpg"
+  },
+  {
+    "revision": "adabe46a238b4de53e877c19881d34de",
+    "url": "/migreat/static/media/winston2.adabe46a.jpg"
+  },
+  {
+    "revision": "a4a065b260f0bb43fe6fe741eeaf183b",
+    "url": "/migreat/static/media/winston3.a4a065b2.jpg"
+  },
+  {
+    "revision": "c0ab643c299441c275e494b7df1b4230",
+    "url": "/migreat/static/media/winston4.c0ab643c.jpg"
+  },
+  {
+    "revision": "d30eb11cdc9b7a914485d79db290088a",
+    "url": "/migreat/static/media/yun1.d30eb11c.jpg"
+  },
+  {
+    "revision": "0f615ef31bd0df203c5d04b71901b13d",
+    "url": "/migreat/static/media/yun2.0f615ef3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//migreat/precache-manifest.de728b8acaadc9c1fbc1017fde7b28df.js"
+  "/migreat/precache-manifest.bdc46bd21ff4c32d8e153498005f37d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//migreat/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/migreat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
